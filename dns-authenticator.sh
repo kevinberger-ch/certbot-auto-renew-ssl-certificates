f836352df851aa368d79e185c9a5de1c6818f97e
Kevin <mail@kevinberger.ch>
2023-02-18 14:33:47 +0100

    Add dns-authenticator.sh

---
+++ dns-authenticator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Variables:
+# CERTBOT_DOMAIN: The domain being authenticated
+# CERTBOT_VALIDATION: The validation string (HTTP-01 and DNS-01 only)
+echo $CERTBOT_DOMAIN
+echo $CERTBOT_VALIDATION
+ssh api@dynv6.com hosts $CERTBOT_DOMAIN records set _acme-challenge txt data $CERTBOT_VALIDATION
+sleep 10
